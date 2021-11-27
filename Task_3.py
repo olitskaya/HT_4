d25bf9185d1ce58964deb29ff77821b6fa8f2fd0
olitskaya <94138631+olitskaya@users.noreply.github.com>
2021-11-27 19:37:10 +0200

    Create Task_3.py

---
+++ Task_3.py
@@ -0,0 +1,21 @@
+# Написати функцію < is_prime >, яка прийматиме 1 аргумент - 
+# число від 0 до 1000, і яка вертатиме True, якщо це число
+# просте, і False - якщо ні.
+
+def is_prime(number):
+    result = True
+    if number == 1:
+        result = False
+    else:          
+        for i in range(2, number):
+            if number % i == 0:
+                state = False
+                break
+    if result == True:
+        print(number, 'is a prime number.')
+    else:
+        print(number, 'is not a prime number.')
+    return result
+    
+test_number = int(input('Enter an integer from 0 to 1000: '))   
+is_prime(test_number)
